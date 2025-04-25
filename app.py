4db8eafd9637f4e229d496b82056e9c2884a5593
Kamil <kamilsiyo@gmail.com>
2025-04-25 18:31:16 +0100

    create index

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/")
+def hello_world():
+   return "<!doctype html><head><meta charset=utf-8><title>Hello World</title></head><body><p>Hello World</p></body>"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
